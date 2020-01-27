db19c5a972837f293ab1a2f2b923a3e489e7515e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-27 16:02:46 +0100

    prettier

--- back/server.ts
+++ back/server.ts
@@ -1,12 +1,11 @@
-import express from 'express';
-import serveIndex from 'serve-index';
+import express from "express";
+import serveIndex from "serve-index";
 const app = express();
 
-const www = '../front/dist/front';
+const www = "../front/dist/front";
 app.use(express.static(www));
 app.use(serveIndex(www, { icons: true }));
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
-
