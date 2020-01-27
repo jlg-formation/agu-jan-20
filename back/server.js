ffab3949dcbe17292b6cdf6ea806f65f23eb2c87
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-27 14:44:42 +0100

    redefine directory back

--- back/server.js
+++ back/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const serveIndex = require('serve-index');
 const app = express();
 
-app.use(express.static('.'));
-app.use(serveIndex('.', { icons: true }));
+const www = '../front/dist/front';
+app.use(express.static(www));
+app.use(serveIndex(www, { icons: true }));
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
