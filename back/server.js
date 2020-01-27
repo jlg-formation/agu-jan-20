2b5f1c9b588d927fb32a7530d976ec689a33f8f2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-27 11:29:53 +0100

    express server done

--- back/server.js
+++ back/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const serveIndex = require('serve-index');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static('.'));
+app.use(serveIndex('.', { icons: true }));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
