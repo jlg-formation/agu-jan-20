0716c938ff3c096098668867250d0fa29dbd2f64
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-28 15:03:30 +0100

    routing ok

--- front/src/app/quizz-create/quizz-create-routing.module.ts
+++ front/src/app/quizz-create/quizz-create-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { SetupComponent } from './setup/setup.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
+import { FinishedComponent } from './finished/finished.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'setup', component: SetupComponent },
+  { path: 'add-question', component: AddQuestionComponent },
+  { path: 'finished', component: FinishedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class QuizzCreateRoutingModule { }
+export class QuizzCreateRoutingModule {}
--- front/src/app/quizz-create/quizz-create.module.ts
+++ front/src/app/quizz-create/quizz-create.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QuizzCreateRoutingModule } from './quizz-create-routing.module';
+import { CreateComponent } from './create/create.component';
+import { SetupComponent } from './setup/setup.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
+import { FinishedComponent } from './finished/finished.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CreateComponent, SetupComponent, AddQuestionComponent, FinishedComponent],
   imports: [
     CommonModule,
     QuizzCreateRoutingModule
