fb9e4546ef02f2839abaf9c5e61e8252dfdf2058
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-29 13:10:02 +0100

    getCuurent

--- front/src/app/services/quizz.service.ts
+++ front/src/app/services/quizz.service.ts
@@ -5,12 +5,27 @@ import { Quizz } from '../entities/quizz';
   providedIn: 'root'
 })
 export class QuizzService {
-
-  current: Quizz;
+  current = this.getCurrent();
   constructor() {}
 
   createCurrent(name: string) {
     this.current = new Quizz();
     this.current.name = name;
+    this.saveCurrent();
+  }
+
+  saveCurrent() {
+    localStorage.setItem('current', JSON.stringify(this.current));
+  }
+
+  getCurrent(): Quizz {
+    const str = localStorage.getItem('current');
+    if (!str) {
+      return undefined;
+    }
+    const q = JSON.parse(str);
+    // q.__proto__ = Quizz.prototype;
+    Object.setPrototypeOf(q, Quizz.prototype);
+    return q;
   }
 }
