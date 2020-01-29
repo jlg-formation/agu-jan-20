44ffe34f0fd8ced18da40054afa72dc88dd0ba2a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-29 16:43:09 +0100

    incrementation qid ok

--- front/src/app/quizz-play/question/question.component.ts
+++ front/src/app/quizz-play/question/question.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizzService } from 'src/app/services/quizz.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question',
@@ -11,11 +12,27 @@ export class QuestionComponent implements OnInit {
   f = new FormGroup({
     givenAnswer: new FormControl('', Validators.required)
   });
-  constructor(public quizz: QuizzService) {}
+  constructor(public quizz: QuizzService, private router: Router) {}
 
   ngOnInit() {}
 
   submit() {
     console.log('submit...');
+    const givenAnswer = this.f.value.givenAnswer;
+    console.log('givenAnswer: ', givenAnswer);
+    const correctAnswer = this.quizz.current.questions[
+      this.quizz.progress.questionId
+    ].correctAnswer;
+    console.log('correctAnswer: ', correctAnswer);
+    if (correctAnswer === givenAnswer) {
+      this.quizz.progress.score++;
+    }
+    this.quizz.progress.questionId++;
+    this.quizz.saveProgress();
+    if (this.quizz.progress.questionId === this.quizz.current.questions.length) {
+      this.router.navigateByUrl('/score');
+      return;
+    }
+    this.f.reset();
   }
 }
